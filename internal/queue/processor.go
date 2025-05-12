062af68bb3ef88a1d0b7cd79acc852af00fa7355
Luiz Gustavo Palmieri <83103347+lgustavopalmieri@users.noreply.github.com>
2025-05-12 09:50:43 -0300

    feat: implements wgs on processor

--- internal/queue/processor.go
+++ internal/queue/processor.go
@@ -1,12 +1,23 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func startProcessors(processors int) {
 	for i := 0; i < processors; i++ {
 		go func(id int) {
 			for msg := range processing {
 				fmt.Printf("⚙️ Handler %d → %s\n", id, msg.Topic)
+				wg := &sync.WaitGroup{}
+				wg.Add(3)
+
+				go saveToDisk(msg, wg)
+				go persistToDB(msg, wg)
+				go sendToCloud(msg, wg)
+
+				wg.Wait()
 
 				mu.Lock()
 				count++
@@ -15,4 +26,20 @@ func startProcessors(processors int) {
 			}
 		}(i)
 	}
+}
+
+func saveToDisk(msg InboundMessage, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Simulate saving to disk
+	fmt.Printf("💾 Saving %s to disk\n", msg.Topic)
+}
+
+func persistToDB(msg InboundMessage, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// simulate DB insert
+}
+
+func sendToCloud(msg InboundMessage, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// simulate cloud pub
 }
